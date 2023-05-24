0df038b98e6165cf8ff0ff4394370eb8c8654310
lukacsg <lukacs.hod@gmail.com>
2023-05-24 13:39:25 +0200

    Update bundle-extra.js
    Add GeoJson to bundle-extra

--- src/bundle-extra.js
+++ src/bundle-extra.js
@@ -9,6 +9,7 @@ import {VectorTile} from '@mapbox/vector-tile';
 
 import {} from './Leaflet.VectorGrid.js';
 import {} from './Leaflet.VectorGrid.Protobuf.js';
+import {} from './Leaflet.VectorGrid.GeoJson.js';
 import {} from './Leaflet.VectorGrid.Slicer.js';
 import {} from './Leaflet.Renderer.Canvas.Tile.js';
 import {} from './Leaflet.Renderer.SVG.Tile.js';
